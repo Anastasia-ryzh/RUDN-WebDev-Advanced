f1fb7262a55dd78e5deb6346137b3231bed957ee
Anastasia-ryzh <nastya.ryzhakova.00@bk.ru>
2026-09-12 16:22:12 +0300

    начало 2 задания

--- client/src/components/NewCardForm/NewCardForm.tsx
+++ client/src/components/NewCardForm/NewCardForm.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import styles from "./NewCardForm.module.css";
+import { useMutation } from "@tanstack/react-query";
 
 export function NewCardForm() {
+const [inputText, setInputText] = useState('');
+
+const mutation = useMutation({ mutationFn: createCard })
+
   return (
     <div className={styles.form}>
-      <input className={styles.input} placeholder="Название карточки" />
+      <input 
+      className={styles.input} 
+      placeholder="Название карточки" 
+      value={inputText}
+      onChange={(event) => setInputText(event.target.value)} 
+      />
       <button className={styles.button} type="button">
         Добавить
       </button>
