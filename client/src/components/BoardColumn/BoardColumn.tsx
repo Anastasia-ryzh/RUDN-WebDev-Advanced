9b75b2099169191d820da23e04fa44b3e6a51823
Anastasia-ryzh <nastya.ryzhakova.00@bk.ru>
2026-09-25 20:59:02 +0300

    feat: filter done cards in BoardColumn

--- client/src/components/BoardColumn/BoardColumn.tsx
+++ client/src/components/BoardColumn/BoardColumn.tsx
@@ -1,19 +1,26 @@
 import type { Card } from "../../types/card";
 import { BoardCard } from "../BoardCard/BoardCard";
 import styles from "./BoardColumn.module.css";
+import { useBoardFiltersStore } from "../../store/boardFiltersStore";
 
 type Props = {
   cards: Card[];
 }
 
 export function BoardColumn(props: Props) {
+  const hideDone = useBoardFiltersStore((state) => state.hideDone);
+
+  const visibleCards = hideDone
+    ? props.cards.filter((card) => !card.isDone)
+    : props.cards;
+
   return (
     <section className={styles.column}>
       <h2 className={styles.title}>К выполнению</h2>
       <div className={styles.cards}>
-        {props.cards.map(card =>
+        {visibleCards.map(card =>
          <BoardCard
-          title={card.title} 
+          title={card.title}
           isDone={card.isDone}
           />)}
       </div>
